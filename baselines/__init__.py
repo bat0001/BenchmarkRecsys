d95afef0d80de5341a957fb0f86b0b22a51c0961
bat0001 <baptiste.bonin10@gmail.com>
2025-05-06 11:17:48 -0400

    add abtest + ucb baselines

--- baselines/__init__.py
+++ baselines/__init__.py
@@ -1,7 +1,12 @@
+from baselines.abtest.abtest_baseline import ABTestBaseline
+from baselines.ucb.ucb_baseline     import UCBBaseline
 from baselines.bandit.linucb_baseline import LinUCBBaseline
 from baselines.gfn.gfn_baseline import ClassicalGFNBaseline
+
 
 BASELINE_REGISTRY = {
     "linucb": LinUCBBaseline,
     "gfn_classical": ClassicalGFNBaseline,
+    "abtest": ABTestBaseline,
+    "ucb":    UCBBaseline,
 }
