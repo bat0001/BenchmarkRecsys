dc8a24c9eb99fb7963fd23fe52c93ee3e17c9a33
bat0001 <baptiste.bonin10@gmail.com>
2025-05-07 15:13:48 -0400

    add TS in registry

--- baselines/__init__.py
+++ baselines/__init__.py
@@ -2,11 +2,12 @@ from baselines.abtest.abtest_baseline import ABTestBaseline
 from baselines.ucb.ucb_baseline     import UCBBaseline
 from baselines.bandit.linucb_baseline import LinUCBBaseline
 from baselines.gfn.gfn_baseline import ClassicalGFNBaseline
-
+from baselines.thompson.baseline import ThompsonBaseline
 
 BASELINE_REGISTRY = {
     "linucb": LinUCBBaseline,
     "gfn_classical": ClassicalGFNBaseline,
     "abtest": ABTestBaseline,
+    "thompson": ThompsonBaseline,
     "ucb":    UCBBaseline,
 }
