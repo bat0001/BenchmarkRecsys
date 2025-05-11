cf96ac2a4e2d09bc9a35886d6ecafcc297654d56
bat0001 <baptiste.bonin10@gmail.com>
2025-05-11 17:38:41 -0400

    add amazon filter dataset inside the amazon module

--- utils/formatters/amazon.py
+++ utils/formatters/amazon.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from utils.formatter.base import BaseFormatter
-from utils.formatter.registry import register
+from utils.formatters.base import BaseFormatter
+from utils.formatters.registry import register
 
 @register("AMAZON")
 class AmazonFormatter(BaseFormatter):
@@ -10,6 +10,12 @@ class AmazonFormatter(BaseFormatter):
             "userId":    "userId",
             "rating":    "rating"
         })
-        # binary reward
         df["reward"] = (df["rating"] > cfg.reward_threshold).astype(int)
+
+        pos_counts = df[df["reward"] == 1]["productId"].value_counts()
+        keep_items = pos_counts[pos_counts >= cfg.amazon_min_pos].index
+        if cfg.amazon_subset is not None:
+            keep_items = keep_items[: cfg.amazon_subset]
+        df = df[df["productId"].isin(keep_items)]
+
         return df[["productId", "userId", "reward"]]
