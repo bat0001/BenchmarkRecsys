e81cc116dda5ecdadc485f3e9fa7316dbffe99ac
bat0001 <baptiste.bonin10@gmail.com>
2025-05-17 00:11:01 -0400

    small change to adapt to the new base class

--- utils/metrics/ctr.py
+++ utils/metrics/ctr.py
@@ -4,6 +4,9 @@ from utils.metrics.registry  import register
 
 @register("CTR")
 class CTRMetric(BaseMetric):
+    def __init__(self, cfg=None):
+        super().__init__(cfg) 
+
     def requires_predictions(self): return False
 
     def __call__(self, canon_df: pd.DataFrame, y_pred, cfg):
