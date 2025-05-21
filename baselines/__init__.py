a78da0ba5b1d7fc372dea181e52795f98099c8ee
bat0001 <baptiste.bonin@epitech.eu>
2025-05-21 11:37:33 -0400

    delete old import

--- baselines/__init__.py
+++ baselines/__init__.py
@@ -1,6 +1,5 @@
 from baselines.abtest.abtest_baseline import ABTestBaseline
 from baselines.ucb.ucb_baseline     import UCBBaseline
-from baselines.bandit.linucb_baseline import LinUCBBaseline
 from baselines.gfn.gfn_baseline import ClassicalGFNBaseline
 from baselines.thompson.baseline import ThompsonBaseline
 from baselines.epsilon.baseline import EpsilonGreedyBaseline
