a0abf8dd034c0dae18f585a32fd14d3911022fd6
bat0001 <baptiste.bonin10@gmail.com>
2025-05-27 22:39:43 -0400

    add BPR baseline in __init__ baseline

--- baselines/__init__.py
+++ baselines/__init__.py
@@ -1,17 +1,17 @@
-from baselines.abtest.abtest_baseline import ABTestBaseline
-from baselines.ucb.ucb_baseline     import UCBBaseline
-from baselines.gfn.gfn_baseline import ClassicalGFNBaseline
-from baselines.thompson.baseline import ThompsonBaseline
-from baselines.epsilon.baseline import EpsilonGreedyBaseline
-from baselines.linucb.baseline import LinUCBBaseline
+# from baselines.abtest.abtest_baseline import ABTestBaseline
+# from baselines.ucb.ucb_baseline     import UCBBaseline
+# from baselines.gfn.gfn_baseline import ClassicalGFNBaseline
+# from baselines.thompson.baseline import ThompsonBaseline
+# from baselines.epsilon.baseline import EpsilonGreedyBaseline
+# from baselines.linucb.baseline import LinUCBBaseline
 from baselines.bpr.baseline import BPRBaseline 
 
 BASELINE_REGISTRY = {
     # "gfn_classical": ClassicalGFNBaseline,
     # "abtest": ABTestBaseline,
-    "thompson": ThompsonBaseline,
+    # "thompson": ThompsonBaseline,
     # "epsilon":   EpsilonGreedyBaseline,
-    "ucb":    UCBBaseline,
-    "linucb":     LinUCBBaseline,
+    #"ucb":    UCBBaseline,
+    #"linucb":     LinUCBBaseline,
     "bpr": BPRBaseline
 }
